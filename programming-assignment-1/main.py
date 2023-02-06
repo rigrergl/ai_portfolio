6912cfd77fee9ab6c996d56006770880bd859d23
rigrergl <rigrergl@gmail.com>
2023-02-05 20:04:06 -0600

    [Programming Assignment 1] Check sys args

--- programming-assignment-1/main.py
+++ programming-assignment-1/main.py
@@ -1,2 +1,15 @@
+import sys
+
 if __name__ == '__main__':
-    print('Hello World')
+    # check if arguments were entered
+    if len(sys.argv) < 3:
+        print("Please enter the algorithm name and relative path to input file")
+        print("Here is an example: ")
+        print("python main.py <algorithm_name> <input_file_path>")
+        quit()
+
+    algorithm_name = sys.argv[1]
+    input_file_path = sys.argv[2]
+
+    print(algorithm_name)
+    print(input_file_path)
