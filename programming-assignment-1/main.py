ccc6f7bf4c525d377907ff19995dbb9ea3ab3d40
rigrergl <rigrergl@gmail.com>
2023-02-05 20:34:24 -0600

    [Programming Assignment 1] Generate state object from input data

--- programming-assignment-1/main.py
+++ programming-assignment-1/main.py
@@ -1,4 +1,29 @@
 import sys
+import pathlib
+
+
+class State:
+    def __init__(self, arg):
+        self.data = None
+        if isinstance(arg, str):
+            self.from_string(arg)
+        elif isinstance(arg, list):
+            self.from_list(arg)
+        else:
+            raise ValueError("Unsupported Argument Type")
+
+    def from_string(self, arg):
+        self.data = []
+        lines = arg.splitlines()
+        for line in lines:
+            self.data.append(line.split())
+
+    def from_list(self, arg):
+        self.data = arg
+
+    def __str__(self):
+        return str(self.data)
+
 
 if __name__ == '__main__':
     # check if arguments were entered
@@ -11,5 +36,10 @@ if __name__ == '__main__':
     algorithm_name = sys.argv[1]
     input_file_path = sys.argv[2]
 
-    print(algorithm_name)
-    print(input_file_path)
+    # Retrieve input data in text format
+    with open(pathlib.Path.cwd().joinpath(input_file_path), 'r') as f:
+        text_in = f.read()
+
+    # Generate State object from input data
+    initial_state = State(text_in)
+    print(initial_state)
