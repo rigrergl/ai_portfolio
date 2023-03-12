d26b0ebb3efc709449e93e346cb5e02b15daef1b
rigrergl <rigrergl@gmail.com>
2023-03-12 16:59:04 -0500

    [pa2] works but only gives one assignment

--- programming-assignment-2/coloring.pl
+++ programming-assignment-2/coloring.pl
@@ -1,4 +1,4 @@
-% coloring(M, G) :- complete(M,G), consistent(M, G)
+coloring(M, G) :- complete(M, G), consistent(M, G).
 
 % define colors
 color(red).
@@ -9,6 +9,16 @@ color(green).
 % define different predicate for colors
 different(X, Y) :- color(X), color(Y), X \= Y.
 
-% ensure that every color has an assignment
+% define the complete predicate
 complete([], [[],[]]).
 complete([paint(X, _) | T], [[X | T1], [_ | T2]]) :- complete(T, [T1 , T2]).
+
+% define the consistent predicate
+consistent(_, [[],[]]).
+consistent(M, [[X | T1], [[Y | T2] | T]]) :- assignment(M, X, Xc), assignment(M, Y, Yc), different(Xc, Yc), consistent(M, [[X | T1], [T2 | T]]).
+consistent(M, [[_ | T], [[] | R]]) :- consistent(M, [T, R]).
+
+
+% define assignment predicate
+assignment([paint(X, Xc) | _], X, Xc).
+assignment([_ | R], X, Xc) :- assignment(R, X, Xc).
